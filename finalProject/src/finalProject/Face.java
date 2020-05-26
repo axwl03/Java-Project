7a66b2bf00a0e0cd57e3aa8dd9683b21decf02a5
axwl03 <arcticwarfare2711@gmail.com>
2020-05-26 08:56:51 +0800

    Add additional two lists to record our own changes and their changes; Modified Face constant

--- finalProject/src/finalProject/Face.java
+++ finalProject/src/finalProject/Face.java
@@ -2,7 +2,8 @@ package finalProject;
 
 /* Define emoji's type constant */
 public class Face {
-	public static final int LAUGH = 1;
-	public static final int CRY = 2;
-	public static final int ANGRY = 3;
+	public static final int ANGRY = 0;
+	public static final int HAPPY= 1;
+	public static final int SAD= 2;
+	public static final int SURPRISE= 3;
 }
--- finalProject/src/finalProject/GameUI.java
+++ finalProject/src/finalProject/GameUI.java
@@ -2,10 +2,14 @@ package finalProject;
 import java.util.ArrayList;
 
 public class GameUI {
-	private ArrayList<Emoji> emojiList;
+	private ArrayList<Emoji> myEmojiList;
+	private ArrayList<Emoji> enemyEmojiList;
+	private ArrayList<Emoji> emojiList;	// original problem
 	private int matchedEmoji;	// store matched result
 	
 	GameUI() {
+		myEmojiList = new ArrayList<Emoji>();
+		enemyEmojiList = new ArrayList<Emoji>();
 		emojiList = new ArrayList<Emoji>();
 	}
 	
