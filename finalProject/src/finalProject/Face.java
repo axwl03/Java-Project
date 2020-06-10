fe793038b8a2d525220fb78ffcfab83ba50b844f
axwl03 <arcticwarfare2711@gmail.com>
2020-06-10 14:39:32 +0800

    Remove finalProject

--- finalProject/src/finalProject/Face.java
+++
@@ -1,9 +0,0 @@
-package finalProject;
-
-/* Define emoji's type constant */
-public class Face {
-	public static final int ANGRY = 0;
-	public static final int HAPPY= 1;
-	public static final int SAD= 2;
-	public static final int SURPRISE= 3;
-}
